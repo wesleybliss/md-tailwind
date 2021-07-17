837bad561ee76e154e49de172cab3977e3388fce
Wesley Bliss <wesley.bliss@gmail.com>
2021-07-17 14:48:36 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import classnames from 'classnames'
 
 import Button from './components/Button/Button'
 import Modal from './components/Modal/Modal'
@@ -9,27 +10,39 @@ import './index.css'
 const App = () => {
     
     const [open, setOpen] = useState(false)
+    const [isDark, setIsDark] = useState(true)
     
     return (
         
         <div className="w-full h-screen flex flex-col">
             
             <header className="w-full p-5 mb-10 bg-gray-300 text-center">
-                <p>
-                    md-tailwind / 1.0
-                </p>
+                <p>md-tailwind / 1.0</p>
             </header>
             
             <div className="flex flex-col justify-center items-center">
-                <Button
-                    className="bg-black"
-                    onClick={() => setOpen(true)}>
-                    Open Dialog
-                </Button>
+                
+                <div className="flex">
+                    <div className="flex flex-col m-5 p-5">
+                        <label>
+                            Dark:
+                            <input type="checkbox" checked={isDark} onClick={() => setIsDark(!isDark)} />
+                        </label>
+                    </div>
+                    <Button
+                        className="bg-black"
+                        onClick={() => setOpen(true)}>
+                        Open Dialog
+                    </Button>
+                </div>
+                
                 <Modal
                     wrapperClassName=""
                     modalClassName=""
-                    modalInnerClassName="bg-gray-900 text-gray-100"
+                    modalInnerClassName={classnames({
+                        'bg-gray-900 text-gray-100': isDark,
+                        'bg-gray-100 text-gray-900': !isDark,
+                    })}
                     overlayClassName=""
                     active={open}
                     toggler={() => setOpen(false)}>
