7d5bc8c2f078b181a539e6889e9342eefeb24c1d
Wesley Bliss <wesley.bliss@gmail.com>
2021-07-20 20:48:11 -0400

    Stories

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,36 +1,138 @@
 import React from 'react'
 
-import { Button } from './Button'
+import Button from './Button'
+
+const colors = [
+    'blueGray',
+    'gray',
+    'brown',
+    'deepOrange',
+    'orange',
+    'amber',
+    'yellow',
+    'lime',
+    'lightGreen',
+    'green',
+    'teal',
+    'cyan',
+    'lightBlue',
+    'blue',
+    'indigo',
+    'deepPurple',
+    'purple',
+    'pink',
+    'red',
+]
 
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-};
+    title: 'Example/Button',
+    component: Button,
+    argTypes: {
+        
+    },
+}
 
-const Template = (args) => <Button {...args} />;
+const Template = (args) => (
+    <Button {...args}>
+        {
+            args.color.substring(0, 1).toUpperCase() +
+            args.color.substring(1)
+        }
+        Button
+    </Button>
+)
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+export const BlueGray = Template.bind({})
+BlueGray.args = {
+    color: 'blueGray',
+}
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+export const Gray = Template.bind({})
+Gray.args = {
+    color: 'gray',
+}
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+export const Brown = Template.bind({})
+Brown.args = {
+    color: 'brown',
+}
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+export const DeepOrange = Template.bind({})
+DeepOrange.args = {
+    color: 'deepOrange',
+}
+
+export const Orange = Template.bind({})
+Orange.args = {
+    color: 'orange',
+}
+
+export const Amber = Template.bind({})
+Amber.args = {
+    color: 'amber',
+}
+
+export const Yellow = Template.bind({})
+Yellow.args = {
+    color: 'yellow',
+}
+
+export const Lime = Template.bind({})
+Lime.args = {
+    color: 'lime',
+}
+
+export const LightGreen = Template.bind({})
+LightGreen.args = {
+    color: 'lightGreen',
+}
+
+export const Green = Template.bind({})
+Green.args = {
+    color: 'green',
+}
+
+export const Teal = Template.bind({})
+Teal.args = {
+    color: 'teal',
+}
+
+export const Cyan = Template.bind({})
+Cyan.args = {
+    color: 'cyan',
+}
+
+export const LightBlue = Template.bind({})
+LightBlue.args = {
+    color: 'lightBlue',
+}
+
+export const Blue = Template.bind({})
+Blue.args = {
+    color: 'blue',
+}
+
+export const Indigo = Template.bind({})
+Indigo.args = {
+    color: 'indigo',
+}
+
+export const DeepPurple = Template.bind({})
+DeepPurple.args = {
+    color: 'deepPurple',
+}
+
+export const Purple = Template.bind({})
+Purple.args = {
+    color: 'purple',
+}
+
+export const Pink = Template.bind({})
+Pink.args = {
+    color: 'pink',
+}
+
+export const Red = Template.bind({})
+Red.args = {
+    color: 'red',
+}
